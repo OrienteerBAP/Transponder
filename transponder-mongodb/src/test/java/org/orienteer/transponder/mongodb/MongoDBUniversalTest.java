70edb47f6b477985820867e0a570ec2d83445748
Ilia Naryzhny <phantom@ydn.ru>
2024-02-15 00:28:12 -0800

    Bump up Mongo version

--- transponder-mongodb/src/test/java/org/orienteer/transponder/mongodb/MongoDBUniversalTest.java
+++ transponder-mongodb/src/test/java/org/orienteer/transponder/mongodb/MongoDBUniversalTest.java
@@ -4,46 +4,37 @@ import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.orienteer.transponder.AbstractUniversalTest;
 
-import com.mongodb.MongoClient;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoDatabase;
 
-import de.flapdoodle.embed.mongo.MongodExecutable;
-import de.flapdoodle.embed.mongo.MongodProcess;
-import de.flapdoodle.embed.mongo.MongodStarter;
-import de.flapdoodle.embed.mongo.config.MongodConfig;
 import de.flapdoodle.embed.mongo.config.Net;
 import de.flapdoodle.embed.mongo.distribution.Version;
-import de.flapdoodle.embed.mongo.tests.MongodForTestsFactory;
-import de.flapdoodle.embed.process.runtime.Network;
+import de.flapdoodle.embed.mongo.transitions.Mongod;
+import de.flapdoodle.embed.mongo.transitions.MongodStarter;
+import de.flapdoodle.embed.mongo.transitions.RunningMongodProcess;
+import de.flapdoodle.reverse.TransitionWalker.ReachedState;
 
 public class MongoDBUniversalTest extends AbstractUniversalTest {
 	
-	private static MongodExecutable mongodExecutable;
+	private static ReachedState<RunningMongodProcess> running;
 	private static MongoClient mongo;
-	private static MongodForTestsFactory mongoFactory;
 	private static MongoDatabase mongoDb;
 	
 	@BeforeClass
 	public static void initMongoDB() throws Exception {
-		MongodStarter starter = MongodStarter.getDefaultInstance();
-
-		int port = Network.freeServerPort(Network.getLocalHost());
-		MongodConfig mongodConfig = MongodConfig.builder()
-		    .version(Version.Main.PRODUCTION)
-		    .net(new Net(port, Network.localhostIsIPv6()))
-		    .build();
-
-		  mongodExecutable = starter.prepare(mongodConfig);
-		  MongodProcess mongod = mongodExecutable.start();
-		  mongo = new MongoClient("localhost", port);
-		  mongoFactory = MongodForTestsFactory.with(Version.Main.PRODUCTION);
-		  mongoDb = mongoFactory.newDatabase(mongo);
+		running = Mongod.instance().start(Version.Main.V5_0);
+		
+		mongo = MongoClients.create("mongodb://"+running.current().getServerAddress());
+		
+		mongoDb = mongo.getDatabase("test-db");
 	}
 	
 	@AfterClass
 	public static void shutdownMongoDB() throws Exception {
-		if(mongoFactory!=null) mongoFactory.shutdown();
-		if(mongodExecutable!=null) mongodExecutable.stop();
+		if(mongoDb!=null) mongoDb.drop();
+		if(mongo!=null) mongo.close();
+		if(running!=null) running.close();
 	}
 	
 	public MongoDBUniversalTest() {
